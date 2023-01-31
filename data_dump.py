0d3b139c3007de936ae1a7bc861c126559f17ed7
sudeep <sujaynyasargi4@gmail.com>
2023-01-31 13:10:22 +0530

    this is our 2nd version code

--- data_dump.py
+++ data_dump.py
@@ -11,4 +11,4 @@ if __name__ == '__main__':
     df.reset_index(drop = True ,inplace = True)
     json_records = list(json.loads(df.T.to_json()).values())
     print(json_records[1])
-    client[Data_base][Colleciton_name].insert_many(json_records) .
+    client[Data_base][Colleciton_name].insert_many(json_records) 
