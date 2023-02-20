a61a66df9f5e1053077520be84d1b69daade1590
sudeep2702 <sujaynyasargi4@gmail.com>
2023-02-20 16:35:10 +0530

    sudeep updated new file

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from sensor.utils import get_collecton_dataframe
 from sensor.entity import config_entity
 from sensor.component.dataingestion import DATA_INGESTION
 from sensor.component.data_validation import DATA_VALIDATION
-
+from sensor.component.data_transformation import DataTransformation  
+from sensor.component.model_train import ModelTrainer
         
 if __name__=="__main__":
 
@@ -24,7 +25,19 @@ if __name__=="__main__":
                         data_ingestion_artifact=data_ingestion_artifact)
 
       data_validation_artifact = data_validation.initiate_data_validation()
+      
 
+   #data_transformation 
+      data_transformation_config  = config_entity.DataTransformationConfig(trainingpipeline_config=training_pipeline_config)
+      data_tranformation = DataTransformation(data_transformation_config=data_transformation_config, data_ingestion_artifact=data_ingestion_artifact)
+      data_transform_art = data_tranformation.initiate_data_transformation()
+
+
+   #model_trainer
+      
+      model_trainerconfig= config_entity.ModelTrainerConfig(trainingpipeline_config=config_entity.TRAINING_PIPELINE_CONGIF())
+      MODEL_TRAINER = ModelTrainer(model_trainer_config=model_trainerconfig , data_transformation_artifact= data_transform_art)
+      model_trainer_art = MODEL_TRAINER.initiate_model_trainer()
 
 
 
--- sensor/config.py
+++ sensor/config.py
@@ -3,11 +3,13 @@ import pandas as pd
 import numpy as np 
 import os
 from dataclasses import dataclass
-
+TARGET_COLUMN = "class"
 
 @dataclass
 class Environment_variable:
     mongodb_url = os.getenv("MONGO_DB_URL")
+
+
 
 env_var = Environment_variable()
 mongo_clent = pymongo.MongoClient(env_var.mongodb_url)
